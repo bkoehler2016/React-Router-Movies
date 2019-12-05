bba59e5eb375fe3194a1954ce993648574f127a1
Ben Koehler <bkoehler85282@gmail.com>
2019-12-04 22:06:34 -0700

    Tried to get info to display when movie clicked on

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,55 +1,43 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import MovieCard from "./MovieCard";
 
-const MovieList = props => {
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    const getMovies = () => {
-      axios
-        .get("http://localhost:5000/api/movies")
-        .then(response => {
-          setMovies(response.data);
-        })
-        .catch(error => {
-          console.error("Server Error", error);
-        });
+export default class MovieList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
     };
+  }
 
-    getMovies();
-  }, []);
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/movies")
+      .then(response => {
+        this.setState(() => ({ movies: response.data }));
+      })
+      .catch(error => {
+        console.error("Server Error", error);
+      });
+  }
 
-  return (
-    <div className="movie-list">
-      {movies.map(movie => (
-        <Link to={`/movies/${movie.id}`}>
+  render() {
+    return (
+      <div className="movie-list">
+        {this.state.movies.map(movie => (
           <MovieDetails key={movie.id} movie={movie} />
-        </Link>
-      ))}
-    </div>
-  );
-};
-
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
+        ))}
       </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
+    );
+  }
 }
 
-export default MovieList;
+function MovieDetails({ movie }) {
+  const { id } = movie;
+  return (
+    <NavLink to={`/movies/${id}`}>
+      <MovieCard movie={movie} />
+    </NavLink>
+  );
+}
